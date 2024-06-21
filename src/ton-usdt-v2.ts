ae24b1ba32a1cecafd8b07ea4e3049f09f32e7a9
realahsanshah <shahahsan56@gmail.com>
2024-06-21 13:01:16 +0500

    updated

---
+++ src/ton-usdt-v2.ts
@@ -0,0 +1,166 @@
+import {
+    WalletContractV4,
+    beginCell,
+    Address,
+    contractAddress,
+    ContractProvider,
+    TonClient4,
+    TonClient,
+    fromNano,
+    toNano,
+    Cell,
+    BitString,
+    Slice,
+} from "@ton/ton";
+import { keyPairFromSecretKey } from "ton-crypto";
+import { SampleJetton } from "./output/sample-jetton";
+import { JettonDefaultWallet } from "./output/sample-jetton-default";
+import { buildOnchainMetadata } from "./utils/jetton-helpers";
+
+// const rpc = 'https://testnet.toncenter.com/api/v2/jsonRPC';
+const rpc = "https://testnet.tonapi.io/";
+// const rpc = "https://sandbox-v4.tonhubapi.com";
+
+const secretKey = "159,25,68,91,70,155,130,104,242,178,123,154,151,60,215,203,158,81,154,125,150,68,113,158,158,254,164,33,56,41,122,180,114,216,56,227,212,167,222,239,153,57,241,228,154,105,154,81,16,183,169,40,201,203,2,226,37,223,163,11,103,184,200,63";
+
+const USDT_CONTRACT_ADDRESS = "EQCKAYNR_4521mPf4-m1UiMqO63l9WEk0ZI_HzPypnxOHkFJ";
+
+
+export const getClient = async (rpc: string) => {
+    try {
+        const client = new TonClient4({
+            endpoint: rpc,
+        });
+
+        return client;
+    }
+    catch (err: any) {
+        console.log(err);
+        throw new Error(err?.message);
+    }
+}
+export const getAccount = async (secretKey: string) => {
+    try {
+        const secretKeyArray = secretKey.split(',').map((x) => parseInt(x));
+        const keyPair = await keyPairFromSecretKey(Buffer.from(secretKeyArray));
+
+        const workchain = 0;
+
+        const wallet = WalletContractV4.create({ workchain, publicKey: keyPair.publicKey });
+
+        console.log('my address', wallet.address);
+
+        return wallet;
+    }
+    catch (e) {
+        console.log(e);
+        throw e;
+    }
+}
+
+const getJettonBalance = async (
+    jettonAddress: string,
+) => {
+    try {
+        // const rpc = "https://testnet.tonapi.io/";
+        // debugger
+        // const client = await getClient(rpc);
+        // debugger
+        // const keyPair = await keyPairFromSecretKey(Buffer.from(secretKey.split(',').map((x) => parseInt(x))));
+        // debugger
+        // const contractAddressInAddress = Address.parseFriendly(jettonAddress);
+        // debugger
+        // const tokenContract = await client.open(SampleJetton.fromAddress(contractAddressInAddress.address));
+
+        // debugger
+
+        // const walletAddress = Address.parseFriendly("0QAeCIcNxWGLUSjU7gyUZkR6RExnDQknpggyUbDvGHDMBadc");
+        debugger
+
+        // const jetton_wallet = await tokenContract.getGetWalletAddress(walletAddress.address);
+        debugger
+        // const contract_dataFormat = JettonDefaultWallet.fromAddress(jetton_wallet);
+        debugger
+        // let contract = client.open(contract_dataFormat);
+        debugger
+        // console.log("Deployer's JettonWallet: " + contract.address);
+        // debugger
+        // let jettonWalletBalance = await (await contract.getGetWalletData()).balance;
+        // debugger
+        // let owner_of_wallet = await (await contract.getGetWalletData()).owner;
+        // debugger
+        // console.log("JettonWallet Balance: " + jettonWalletBalance);
+        // console.log("JettonWallet Owner: \n" + owner_of_wallet);
+        debugger
+        // let contract = client.open(contract_dataFormat);
+        // console.log("Deployer's JettonWallet: " + contract.address);
+
+
+
+        // reference code
+        const rpc = "https://testnet.tonapi.io/";
+        debugger
+        const client = await getClient(rpc);
+        const workchain = 0;
+        debugger
+        let deploy_wallet = WalletContractV4.create({ workchain: 0, publicKey: keyPair.publicKey });
+        debugger
+        let deploy_wallet_contract = client.open(deploy_wallet);
+        debugger
+        // Get deployment wallet balance
+        // let balance: bigint = await deploy_address.getBalance();
+        const jettonParams = {
+            name: "Test 123 Best Practice",
+            description: "This is description of Test tact jetton",
+            symbol: "PPPPPPPP",
+            image: "https://cdn.logo.com/hotlink-ok/logo-social.png",
+        };
+        debugger
+        let max_supply = toNano(1234567666666689011); // Set the specific total supply in nano
+        debugger
+        // Create content Cell
+        let content = buildOnchainMetadata(jettonParams);
+        debugger
+        let init = await SampleJetton.init(deploy_wallet_contract.address, content, max_supply);
+        debugger
+        let jetton_minter_contract_address = contractAddress(workchain, init);
+        console.log("Jetton Master: " + jetton_minter_contract_address);
+        debugger
+        let contract_ddd = await client.open(SampleJetton.fromAddress(jetton_minter_contract_address));
+        debugger
+        let jetton_wallet = await contract_ddd.getGetWalletAddress(deploy_wallet_contract.address);
+        debugger
+        let contract_dataFormat = JettonDefaultWallet.fromAddress(jetton_wallet);
+        debugger
+        let contract = client.open(contract_dataFormat);
+        console.log("Deployer's JettonWallet: " + contract.address);
+        debugger
+        let jettonWalletBalance = await (await contract.getGetWalletData()).balance;
+        debugger
+        let owner_of_wallet = await (await contract.getGetWalletData()).owner;
+        debugger
+        console.log("JettonWallet Balance: " + jettonWalletBalance);
+        console.log("JettonWallet Owner: \n" + owner_of_wallet);
+        debugger
+        // TODO:
+        // // loadOwnershipAssigned => msg.forwardload
+        // let aa = loadTransferEvent(src.asSlice());
+        // console.log("Mint MemberID: " + aa.item_index + ", by " + aa.minter);
+
+        // reference code end
+
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+(async () => {
+    const wallet = await getAccount(secretKey);
+
+    debugger
+
+    const jettonBalance = await getJettonBalance(USDT_CONTRACT_ADDRESS);
+
+    debugger
+})()
